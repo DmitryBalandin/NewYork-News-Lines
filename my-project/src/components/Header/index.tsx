899c5f5251eda839c0a189157a0d9bc7d3334a1f
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-09-14 19:37:35 +0500

    feat: burgerMenu working

--- my-project/src/components/Header/index.tsx
+++ my-project/src/components/Header/index.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './styles.module.scss'
 
 interface Header {
 
 }
-
+const nameTheme: Array<string> = ['1', '2', '3', '4', '5']
 export const Header: React.FC<Header> = () => {
+    const [isOpenMenu, setIsOpenMenu] = useState(false)
+    const handleCheckbox = () => setIsOpenMenu((isOpen) => !isOpen)
     return (
         <header className={styles.burger}>
-            <div className={styles.burgerContainer}>
-                <label className={styles.burgerContainerLabel} htmlFor="input-burger">asdasd   </label>
-                <input className={styles.burgerContainerCheckbox} type="checkbox" id="input-burger" />
+            <div className={styles.burgerControl}>
+                <input className={styles.burgerCheckbox} type="checkbox" id="input-burger"  />
+                <label className={styles.burgerLabel} htmlFor="input-burger" ></label>
             </div>
             <h2 className={styles.burgerTitle}>BESIDER</h2>
+            <div className={styles.burgerContainer}  style={!isOpenMenu ? { transform: 'translateX(-100%)' } : { transform: 'translateX(0%)' } }>
+                <ul className={styles.burgerMenu}>
+                    {nameTheme.map((theme, index) => {
+                        return <li key={index}>{theme}</li>
+                    })}
+                </ul>
+            </div>
         </header>
     )
 }
